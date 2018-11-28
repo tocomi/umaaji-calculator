bd791bba534cc246ece16c5c66bac5f8e9e1a548
tocomi <tocomi@wish.ocn.ne.jp>
2018-11-29 02:57:42 +0900

    show past races

---
+++ src/components/Horse.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  div.horse
+    div#info
+      span#name {{ horse.horse_name }}
+    Race(v-for="race in horse.past_races" :key="race.grade" :race="race")
+</template>
+
+<script>
+import Race from './Race.vue'
+
+export default {
+  props: {
+    horse: Object,
+  },
+  components: {
+    Race: Race,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.horse {
+  width: 950px;
+  height: 100px;
+  #info {
+    float: left;
+    width: 130px;
+    height: 80px;
+    padding: 10px;
+    background-color: #DDD;
+  }
+}
+</style>
