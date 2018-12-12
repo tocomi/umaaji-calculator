105089fbba2e5268485439ac107287678beb6454
tocomi <tocomi@wish.ocn.ne.jp>
2018-12-13 01:54:42 +0900

    change style

--- src/components/Race.vue
+++ src/components/Race.vue
@@ -1,16 +1,27 @@
 <template lang="pug">
-  div.race
+  div.race(:class="{ rest: isRest }")
     span#name {{ race.name }} {{ race.grade }}
     br
     span#condition {{ race.ground }} {{ race.distance }} {{ race.status }}
     br
-    span#time {{ race.time }} ({{ race.diff }})
+    span#time {{ race.time }} {{ diff }}
 </template>
 
 <script>
 export default {
   props: {
     race: Object,
+  },
+  computed: {
+    diff() {
+      if (this.race.diff) {
+        return '(' + this.race.diff + ')'
+      }
+      return ''
+    },
+    isRest() {
+      return this.race.name.includes('休養')
+    }
   }
 }
 </script>
@@ -22,5 +33,8 @@ export default {
   height: 80px;
   padding: 10px;
   border: solid 1px #AAA;
+}
+.rest {
+  background-color: #DDD;
 }
 </style>
