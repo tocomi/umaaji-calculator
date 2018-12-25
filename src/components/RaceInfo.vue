c8f79e768d2f58ca4706bd95049df36dbac00f3f
tocomi <tocomi@wish.ocn.ne.jp>
2018-12-26 02:05:42 +0900

    enable to select race

---
+++ src/components/RaceInfo.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  button#race_info
+    span {{ race.name }} {{ race.grade }}
+    br
+    span {{ race.ground }} {{ race.distance }}
+</template>
+
+<script>
+export default {
+  props: {
+    race: Object
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#race_info {
+  height: 55px;
+  width: 200px;
+  margin-top: 10px;
+  outline: 0;
+  background-color: #394C82;
+  color: #FFFFFF;
+  border-radius: 20px;
+  &:hover {
+    background-color: #5B6EA4;
+  }
+}
+</style>
