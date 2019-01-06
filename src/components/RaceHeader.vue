5a60dd54f4f14a24b7ef620a07de0ad7947ab2d4
tocomi <tocomi@wish.ocn.ne.jp>
2019-01-06 17:14:03 +0900

    show race info header

---
+++ src/components/RaceHeader.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div#race_header
+    span {{ race.place }}{{ race.round }}R {{race.ground}}{{ race.distance }} {{ race.name}}
+</template>
+
+<script>
+export default {
+  props: {
+    race: Object,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+#race_header {
+  background-color: #394C82;
+  color: #FFF;
+  font-size: 30px;
+}
+</style>
