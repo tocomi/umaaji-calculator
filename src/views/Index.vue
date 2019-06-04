3a74419f1ed2b266a997564e835a4b92778d94a0
tocomi <tocomi@wish.ocn.ne.jp>
2019-06-05 01:48:35 +0900

    show mobile menu

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,8 +2,7 @@
   div#index
     v-layout
       v-flex(hidden-sm-and-up)
-        v-toolbar
-          v-btn(flat) races
+        RaceList(:loading="loading" :racePlace="racePlace" @setHorseData="setHorseData")
     v-progress-circular.circular(indeterminate color="primary" v-if="loading")
     v-layout
       v-flex#list-flex(hidden-xs-only lg2)
