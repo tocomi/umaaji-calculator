167cf189adfe1f7a48ef47411a702441baf89abe
tocomi <tocomi@wish.ocn.ne.jp>
2019-11-16 01:03:05 +0900

    test: setup babel

--- test/components/Loading.spec.js
+++ test/components/Loading.spec.js
@@ -1,6 +1,9 @@
 import { mount } from '@vue/test-utils'
+import Loading from '@/components/Loading.vue'
 
 describe('Component', () => {
   test('is a Vue instance', () => {
+    const wrapper = mount(Loading)
+    expect(wrapper.element).toMatchSnapshot()
   })
 })
